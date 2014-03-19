4a120316a2c84e1e110ac31fc12e3a8e93dd146f
ea0723 <elenanewton@icloud.com>
2014-03-19 15:11:16 +0800

    updated app > admin > Company to resolve viewing issues

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -8,27 +8,29 @@ ActiveAdmin.register Company do
     column :state
     column :industry
     column "Created", :created_at
-    column "Last Modified", :modified_at
+    column "Last Modified", :updated_at
     actions
   end
 
   filter :size
   filter :state
   filter :industry
+  filter :min_employees
+  filter :max_employees
   filter :created_at
+  filter :updated_at
 
 
-  show do
+  show :title => :name do
     attributes_table do
       row :name
-#      row do
-#        column :city+:state.join(", "), :label => "Location"
-#      end
+      row :city
+      row :state
       row :industry
       row :min_employees
       row :max_employees
       row :created_at, :label=> "Created"
-      row :modified_at, :label => "Last Modified"
+      row :updated_at, :label => "Last Modified"
     end
   end
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ActiveRecord::Base
-  attr_accessible :name, :city, :state, :industry, :size, :min_employees, :max_employees
+  attr_accessible :name, :city, :state, :industry, :size, :min_employees, :max_employees, :created_at, :modified_at
+
+
 
 end
