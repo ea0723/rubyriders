237d5f6fe1a2cf151a9b74e078442dc2b9bc3b14
ea0723 <elenanewton@icloud.com>
2014-04-16 23:03:27 +0800

    resolves simple_form form_horizontal issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-
+    @info_request = InfoRequest.new
+    render 'home/index'
   end
+
+  def create
+    request = InfoRequest.new(params[:info_request])
+    request.save!
+    redirect_to :controller => :info_requests, :action => :index
+  end
+
 end
